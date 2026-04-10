7788dc60b3febca6937da21fa189fcaa0e5aba1a
fatima-latif <fatima.latif@camp1.tkxel.com>
2026-04-10 16:07:51 +0500

    added the format command

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ module "vnet" {
   source           = "./module/vnet"
   project          = local.project
   env              = local.env
-  location         = "East US"//module.resourceGroup.rgLocation
-  rgGroup          = "kml_rg_main-9299b955689d4ad0"//module.resourceGroup.resourceGroup
+  location         = "East US"                      //module.resourceGroup.rgLocation
+  rgGroup          = "kml_rg_main-9299b955689d4ad0" //module.resourceGroup.resourceGroup
   cidrRange        = ["10.0.0.0/16"]
   publicCidrRange  = ["10.0.1.0/24"]
   privateCidrRange = ["10.0.10.0/24"]
@@ -24,19 +24,19 @@ module "webSecurityGroup" {
   source   = "./module/securityGroup"
   project  = local.project
   env      = local.env
-  location         = "East US"//module.resourceGroup.rgLocation
-  rgGroup          = "kml_rg_main-9299b955689d4ad0"//module.resourceGroup.resourceGroup
-#   location = module.resourceGroup.rgLocation
-#   rgGroup  = module.resourceGroup.resourceGroup
+  location = "East US"                      //module.resourceGroup.rgLocation
+  rgGroup  = "kml_rg_main-9299b955689d4ad0" //module.resourceGroup.resourceGroup
+  #   location = module.resourceGroup.rgLocation
+  #   rgGroup  = module.resourceGroup.resourceGroup
 }
 
 module "virtualMachine" {
-  source         = "./module/vm"
-  project        = local.project
-  env            = local.env
-  location         = "East US"//module.resourceGroup.rgLocation
-  rgGroup          = "kml_rg_main-9299b955689d4ad0"//module.resourceGroup.resourceGroup
-#   rgGroup        = module.resourceGroup.resourceGroup
+  source   = "./module/vm"
+  project  = local.project
+  env      = local.env
+  location = "East US"                      //module.resourceGroup.rgLocation
+  rgGroup  = "kml_rg_main-9299b955689d4ad0" //module.resourceGroup.resourceGroup
+  #   rgGroup        = module.resourceGroup.resourceGroup
   userName       = "useradmin"
   passWord       = "8weR@888888"
   publicSubnetId = module.vnet.publicSubnetId
@@ -47,8 +47,8 @@ module "webBlobStorage" {
   source   = "./module/blobStorage"
   project  = local.project
   env      = local.env
-  location         = "East US"//module.resourceGroup.rgLocation
-  rgGroup          = "kml_rg_main-9299b955689d4ad0"//module.resourceGroup.resourceGroup
-#   location = module.resourceGroup.rgLocation
-#   rgGroup  = module.resourceGroup.resourceGroup
+  location = "East US"                      //module.resourceGroup.rgLocation
+  rgGroup  = "kml_rg_main-9299b955689d4ad0" //module.resourceGroup.resourceGroup
+  #   location = module.resourceGroup.rgLocation
+  #   rgGroup  = module.resourceGroup.resourceGroup
 }
