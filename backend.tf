7f71aaab2b0c1c6b77e6ac9133f6e717cf45ae6a
fatima-latif <fatima.latif@camp1.tkxel.com>
2026-04-10 16:41:31 +0500

    updated the remote bakend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "kml_rg_main-7f95962493ea46f6"
-    storage_account_name = "tfbackendapp"
+    storage_account_name = "backendtfapp"
     container_name       = "backendfile"
     key                  = "terraform.tfstate"
   }
